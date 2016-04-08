bf7f55134896e1cceb3ebf0c7dfac92c1bdd71cc
GeorgeErickson <george.erickson.jr@gmail.com>
2016-04-07 23:58:29 -0400

    Setup

--- main_test.go
+++ main_test.go
@@ -1,33 +1,55 @@
 package main
 
 import (
+	"reflect"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/k0kubun/pp"
 )
 
-type Message struct {
-	RemoteAddr string
-	UserId     string
-	UserName   string
-	Time       string
-	Request    string
-	StatusCode string
-	Size       string
+type Parser struct {
+	timeFormat string
 }
 
-func LogFields(l string) []string {
-	a := make([]string, 7)
+type Message struct {
+	RemoteAddr    string
+	UserId        string
+	RemoteUser    string
+	Time          time.Time
+	Request       string
+	Status        string
+	BodyBytesSent string
+}
+
+func (m *Message) set(i int, s string) {
+	switch i {
+	case 3:
+		t, err := time.Parse("02/Jan/2006:15:04:05 -0700", s)
+		m.Time = t
+		if err != nil {
+			panic(err)
+		}
+	default:
+		reflect.ValueOf(m).Elem().Field(i).SetString(s)
+	}
+}
+
+func LogFields(l string) *Message {
+	// a := make([]string, 7)
 	na := 0
 	fieldStart := -1
 	mode := ' '
+
+	msg := &Message{}
+	// rmsg := reflect.ValueOf(msg).Elem()
 
 	for i, r := range l {
 		switch r {
 		case mode:
 			if fieldStart >= 0 {
-				a[na] = l[fieldStart:i]
+				msg.set(na, l[fieldStart:i])
 				na++
 				fieldStart = -1
 			}
@@ -45,15 +67,30 @@ func LogFields(l string) []string {
 		}
 	}
 	if fieldStart >= 0 { // Last field might end at EOF.
-		a[na] = l[fieldStart:]
+		msg.set(na, l[fieldStart:])
 	}
-	return a
+	return msg
+}
+
+func LogFields2(l, f string) *Message {
+	msg := &Message{}
+
+	return msg
 }
 
 func BenchmarkFields(b *testing.B) {
+
 	l := `127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326`
 	for i := 0; i < b.N; i++ {
 		strings.Fields(l)
+	}
+}
+
+func BenchmarkLogFields2(b *testing.B) {
+	f := `%h %l %u [%t] "%r" %>s %b`
+	l := `127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326`
+	for i := 0; i < b.N; i++ {
+		LogFields2(l, f)
 	}
 }
 
