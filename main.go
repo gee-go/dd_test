8540a33c790c4f3f6a972d6fa3f70a92ac3f6f0a
GeorgeErickson <george.erickson.jr@gmail.com>
2016-04-08 18:27:33 -0400

    Cleanup

--- main.go
+++ main.go
@@ -1,1 +1,83 @@
 package main
+
+import (
+	"reflect"
+	"time"
+)
+
+const (
+	// DefaultLogFormat is a format string for the common log format
+	DefaultLogFormat = `{remote} {ident} [{time}] "{method} {uri} {proto}" {status} {size}`
+
+	// DefaultTimeFormat is the default format string used to parse timestamps
+	DefaultTimeFormat = "02/Jan/2006:15:04:05 -0700"
+)
+
+// Message represents a single log line.
+type Message struct {
+	Remote string
+	Ident  string
+	Auth   string
+	Time   time.Time
+
+	// Request
+	Method string
+	URI    string
+	Proto  string
+
+	Status string
+	Size   string
+}
+
+func (m *Message) set(i int, s string) {
+	switch i {
+	case 3:
+		t, err := time.Parse(DefaultTimeFormat, s)
+		m.Time = t
+		if err != nil {
+			panic(err)
+		}
+	default:
+		reflect.ValueOf(m).Elem().Field(i).SetString(s)
+	}
+}
+
+// TODO - check for urls that contain [] or ""
+func ParseLine(l string) *Message {
+	na := 0
+	fieldStart := -1
+	mode := ' '
+
+	msg := &Message{}
+
+	for i, r := range l {
+		switch r {
+		case mode:
+			if fieldStart >= 0 {
+				msg.set(na, l[fieldStart:i])
+				na++
+				fieldStart = -1
+			}
+			mode = ' '
+		case '[':
+			if mode == ' ' {
+				fieldStart = -1
+				mode = ']'
+			}
+
+		case '"':
+			if mode == ' ' {
+				fieldStart = -1
+				mode = '"'
+			}
+		default:
+			if fieldStart == -1 {
+				fieldStart = i
+			}
+		}
+	}
+	if fieldStart >= 0 { // Last field might end at EOF.
+		msg.set(na, l[fieldStart:])
+	}
+	return msg
+}
--- main_test.go
+++ main_test.go
@@ -2,87 +2,17 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 	"strings"
 	"testing"
-	"time"
 	"unicode"
 
 	"github.com/k0kubun/pp"
 	"github.com/stretchr/testify/require"
 )
 
-type Parser struct {
-	timeFormat string
-}
-
-type Message struct {
-	Remote        string
-	Ident         string
-	Auth          string
-	Time          time.Time
-	Request       string
-	Status        string
-	BodyBytesSent string
-}
-
-func (m *Message) set(i int, s string) {
-	switch i {
-	case 3:
-		t, err := time.Parse("02/Jan/2006:15:04:05 -0700", s)
-		m.Time = t
-		if err != nil {
-			panic(err)
-		}
-	default:
-		reflect.ValueOf(m).Elem().Field(i).SetString(s)
-	}
-}
-
 var delimMap = map[rune]rune{
 	'[': ']',
 	'"': '"',
-}
-
-// TODO - check for urls that contain [] or ""
-func LogFields(l string) *Message {
-	// a := make([]string, 7)
-	na := 0
-	fieldStart := -1
-	mode := ' '
-
-	msg := &Message{}
-
-	for i, r := range l {
-		switch r {
-		case mode:
-			if fieldStart >= 0 {
-				msg.set(na, l[fieldStart:i])
-				na++
-				fieldStart = -1
-			}
-			mode = ' '
-		case '[':
-			if mode == ' ' {
-				fieldStart = -1
-				mode = ']'
-			}
-
-		case '"':
-			if mode == ' ' {
-				fieldStart = -1
-				mode = '"'
-			}
-		default:
-			if fieldStart == -1 {
-				fieldStart = i
-			}
-		}
-	}
-	if fieldStart >= 0 { // Last field might end at EOF.
-		msg.set(na, l[fieldStart:])
-	}
-	return msg
 }
 
 type Field struct {
@@ -133,17 +63,17 @@ func TestLogFields2(t *testing.T) {
 func BenchmarkLogFields(b *testing.B) {
 	l := `127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326`
 	for i := 0; i < b.N; i++ {
-		LogFields(l)
+		ParseLine(l)
 	}
 }
 
 func TestMain(t *testing.T) {
 	l := `127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326`
-	pp.Println(LogFields(l))
+	pp.Println(ParseLine(l))
 
 	l2 := `127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif?a=[ HTTP/1.0" 200 2326`
-	msg := LogFields(l2)
+	msg := ParseLine(l2)
 	assert := require.New(t)
-	assert.Equal("GET /apache_pb.gif?a=[ HTTP/1.0", msg.Request)
+	assert.Equal("GET /apache_pb.gif?a=[ HTTP/1.0", msg.Method)
 
 }
