fe933d9b12fc11d3eade98304cb77d8a32a4f8d0
GeorgeErickson <george.erickson.jr@gmail.com>
2016-04-11 18:02:45 -0400

    Cleanup parser

---
+++ src/lparse/buffer.go
@@ -0,0 +1,63 @@
+package lparse
+
+import (
+	"unicode"
+	"unicode/utf8"
+)
+
+type Buffer struct {
+	s   string
+	pos int
+}
+
+func (b *Buffer) Init(s string) {
+	b.s = s
+	b.pos = 0
+}
+
+func (b *Buffer) advanceUntil(end rune) string {
+	var r rune
+	var v string
+
+	for i, w := b.pos, 0; i < len(b.s); i += w {
+		r, w = utf8.DecodeRuneInString(b.s[i:])
+
+		// special case for space to account for unicode.
+		if r == end || (end == ' ' && unicode.IsSpace(r)) {
+			v = b.s[b.pos:i]
+			b.pos = i + w
+			break
+		}
+
+	}
+
+	if v == "" {
+		v = b.s[b.pos:]
+	}
+
+	b.skipDelim()
+
+	return v
+}
+
+func (b *Buffer) skipDelim() {
+	var r rune
+	for i, w := b.pos, 0; i < len(b.s); i += w {
+		r, w = utf8.DecodeRuneInString(b.s[i:])
+
+		if isDelim(r) {
+			b.pos += w
+		} else {
+			return
+		}
+	}
+}
+
+func isDelim(r rune) bool {
+	switch r {
+	case ' ', ']', '"', '[':
+		return true
+	}
+
+	return unicode.IsSpace(r)
+}
--- src/lparse/parser.go
+++ src/lparse/parser.go
@@ -1,77 +1,20 @@
 package lparse
 
-import (
-	"unicode"
-	"unicode/utf8"
-)
-
 // Parser is used to convert lines to Message structs
 type Parser struct {
-	s   string
-	pos int
-	end rune
-
+	b      *Buffer
 	config *Config
 }
 
 func New(c *Config) *Parser {
-	return &Parser{config: c}
-}
-
-// parseUntil scans through the log line util the fn returns true.
-func (p *Parser) parseUntil(fn func(rune) bool) string {
-	var v string
-	var r rune
-
-	// scan until fn return true
-	for i, w := p.pos, 0; i < len(p.s); i += w {
-		r, w = utf8.DecodeRuneInString(p.s[i:])
-
-		if fn(r) {
-			v = p.s[p.pos:i]
-			p.pos = i + w
-			break
-		}
-
-	}
-
-	// at end of string
-	if v == "" {
-		return p.s[p.pos:]
-	}
-
-	// skip delim chars.
-	for i, w := p.pos, 0; i < len(p.s); i += w {
-		r, w = utf8.DecodeRuneInString(p.s[i:])
-
-		if r == ' ' || r == ']' || r == '"' || r == '[' || unicode.IsSpace(r) {
-			p.pos += w
-		} else {
-			return v
-		}
-	}
-
-	return v
-}
-
-func (p *Parser) parse() string {
-	if unicode.IsSpace(p.end) {
-		return p.parseUntil(unicode.IsSpace)
-	}
-
-	s := p.parseUntil(func(r rune) bool {
-		return r == p.end
-	})
-
-	return s
+	return &Parser{config: c, b: &Buffer{}}
 }
 
 // Parse converts a line to a message.
 func (p *Parser) Parse(l string) (*Message, error) {
-	p.pos = 0
-	p.end = ' '
-	p.s = l
+	p.b.Init(l)
 
+	end := ' '
 	fieldStart := -1
 	prev := ' '
 	msg := &Message{}
@@ -83,14 +26,14 @@ func (p *Parser) Parse(l string) (*Message, error) {
 			fieldStart = i + 1
 			switch prev {
 			case '[':
-				p.end = ']'
+				end = ']'
 			case '"':
-				p.end = '"'
+				end = '"'
 			default:
-				p.end = prev
+				end = prev
 			}
 		case '}':
-			if err := msg.set(p.config.LogFormat[fieldStart:i], p.parse()); err != nil {
+			if err := msg.set(p.config.LogFormat[fieldStart:i], p.b.advanceUntil(end)); err != nil {
 				return msg, err
 			}
 		}
